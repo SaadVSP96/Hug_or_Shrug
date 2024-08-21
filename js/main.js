a798a383f642aebf0d279c239b6c707524844b6d
SaadVSP96 <saadmasood192@gmail.com>
2024-08-21 17:31:37 +0500

    14. Perfected Drop Down Functionality

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const toggler = document.querySelector(".nav__toggler");
-    const menuBar = document.querySelector(".nav__list");
-    menuBar.classList.remove("collapsible__content");
     toggler.addEventListener("click", function () {
-        // console.log(menuBar.classList);
-        if (menuBar.classList.contains("collapsible__content")) {
-            menuBar.classList.remove("collapsible__content");
-        } else {
+        const menuBar = document.querySelector(".nav__list");
+        console.log(menuBar.classList);
+        if (!menuBar.classList.contains("collapsible__content")) {
             menuBar.classList.add("collapsible__content");
+        } else {
+            menuBar.classList.remove("collapsible__content");
         }
     });
 });
